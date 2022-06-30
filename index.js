24a0122cbd949de495aca17c8de9dbb382decf78
Kaniz Fatema <kaniz.fatema@bitopibd.com>
2022-07-01 00:59:56 +0600

    task deletion done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -31,7 +31,7 @@ async function run(){
         console.log('db connected')
 
 
-
+        //Send task to db
         app.post("/task", async(req,res)=>{
             const task = req.body;
             console.log(task)
@@ -39,12 +39,26 @@ async function run(){
             res.send(result)
         })
 
+        //Get tasks from db
+
         app.get("/gettask", async(req,res)=>{
             const query = {}
             const cursor = taskCollection.find(query);
             const task = await cursor.toArray();
             res.send(task)
         })
+
+        //Edit tasks
+
+
+        //Delete Tasks
+
+        app.delete("/task/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = taskCollection.deleteOne(query);
+            res.send(result);
+          });
 
         
 
